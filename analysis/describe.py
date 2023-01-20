d47fa24d2e09ce4f91298e4cd2e9239b362ced87
César Claude <cesarclaudepro@gmail.com>
2023-01-20 17:10:53 +0100

    :sparkles: done with describe function

--- analysis/describe.py
+++ analysis/describe.py
@@ -2,6 +2,7 @@ import sys, csv, math
 import pandas as pd
 import numpy as np
 
+# Utils
 def is_float(element: any) -> bool:
     if element is None: 
         return False
@@ -10,20 +11,7 @@ def is_float(element: any) -> bool:
         return True
     except ValueError:
         return False
-
-
-def percentile (data : pd.DataFrame, percentile : float) -> float:
-    # print(f"p {percentile} -> {(data.size - 1) * percentile}")
-    i = float(percentile * (data.size - 1))
-    if i.is_integer():
-        return data.iloc[int(i)]
-    else:
-        print(i, 1 - i % 1, i % 1, math.floor(i), math.ceil(i), data.iloc[math.floor(i)], data.iloc[math.ceil(i)], data.iloc[math.floor(i)] * (1 - i % 1) + data.iloc[math.ceil(i)] * (i % i))
-    return 0
-
-
-
-
+    
 def read_file (filename : str) -> pd.DataFrame:
     with open(filename) as csvfile:
         csvdata = csv.reader(csvfile)
@@ -33,41 +21,79 @@ def read_file (filename : str) -> pd.DataFrame:
         return df
     return None
 
-# def normalize (df : pd.DataFrame) -> pd.DataFrame:
-#     normalized = pd.DataFrame()
-#     for col in df:
-#         if is_float(df[col].iloc[0]):
-#             print(df[col].replace('', np.nan).dropna().size)
-#             normalized[col] = df[col].replace('', np.nan).dropna()
-#             print("normed", normalized[col].size)
-#     # print(normalized)
-#     return normalized
+
+# Analyze functions
+def mean (data : pd.DataFrame):
+    tot = 0
+    for i in range(data.size):
+        tot += data.iloc[i]
+    return tot / data.size
+
+def stddev (data : pd.DataFrame, mean : float):
+    tot = 0
+    for i in range(data.size):
+        tot += (data.iloc[i] - mean) ** 2
+    return math.sqrt(tot / data.size)
+
+def percentile (data : pd.DataFrame, percentile : float) -> float:
+    i = float(percentile * (data.size - 1))
+    if i.is_integer():
+        return data.iloc[int(i)]
+    else:
+        return data.iloc[math.floor(i)] * (1 - i % 1) + data.iloc[math.ceil(i)] * (i % 1)
 
 def analyze_feature (feature : pd.DataFrame) -> dict:
     feature = feature.replace('', np.nan).dropna().astype(float).sort_values()
-    print(feature)
+    m = mean(feature)
     return {
         "name": feature.name,
         "count": feature.size,
-        "mean": 0 / feature.size,
-        "std": 0,
+        "mean": m,
+        "std": stddev(feature, m),
         "min": percentile(feature, 0),
-        "lower": percentile(feature, 0.25),
-        "median": percentile(feature, 0.5),
-        "upper": percentile(feature, 0.75),
-        "max": percentile(feature, 1),
+        "25%": percentile(feature, 0.25),
+        "50%": percentile(feature, 0.5),
+        "75%": percentile(feature, 0.75),
+        "max": percentile(feature, 1)
     }
 
 
+# Display
+def display (data):
+    SIZE = 4
+    # For each block of SIZE
+    for i in range(0, len(data), SIZE):
+        slce = data[i:i+SIZE]
+
+        # NAME line
+        line = f"{0:<6}"
+        for desc in slce:
+            if len(desc['name']) > 14:
+                line += f"{desc['name'][:11]:>13s}..."
+            else:
+                line += f"{desc['name']:>16s}"
+        print(line)
+
+        # For each legend...
+        for legend in ["count", "mean", "std", "min", "25%", "50%", "75%", "max"]:
+            # LEGEND line
+            line = f"{legend:6s}"
+            for desc in slce:
+                if -1000000 <= desc[legend] <= 1000000:
+                    line += f"{desc[legend]:16.6f}"
+                else:
+                    line += f"{desc[legend]:16.6e}"
+            print(line)
+
+        print()
+
+
+# Describe
 def describe (filename : str):
-    descriptions = []
-
     df = read_file(filename)
-    # df = normalize(df)
-    descriptions = [analyze_feature(df[col]) for col in df if is_float(df[col].iloc[0]) and df[col].name == "Arithmancy"]
 
-    # print(pd.DataFrame.describe(df.astype(float)))
-    print(descriptions)
+    descriptions = [analyze_feature(df[col]) for col in df if is_float(df[col].iloc[0])]
+    display(descriptions)
 
 def main ():
     if len(sys.argv) > 1:
@@ -75,7 +101,6 @@ def main ():
     else:
         print("Please provide a CSV file to read :", file=sys.stderr)
         print("    python3 describe.py filename.csv", file=sys.stderr)
-
 
 if __name__ == "__main__":
     main()
